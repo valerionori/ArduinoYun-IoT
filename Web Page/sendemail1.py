9b164f40d09dfe00d69d11fc15c701edad35fcd5
valerionori <114622088+valerionori@users.noreply.github.com>
2022-09-29 00:36:35 +0200

    Add files via upload

---
+++ Web Page/sendemail1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import smtplib
+
+sender = 'networking.project.test@gmail.com'
+receivers = ['valerio.nori@hotmail.com']
+passwd = 'Dbhx56hs_'
+message = 'SILOS -- 1 -- IS EMPTY!'
+
+
+smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.ehlo()
+#smtpObj.set_debuglevel(0)
+print "1"
+smtpObj.login(sender,passwd)
+smtpObj.sendmail(sender, receivers, message)
+smtpObj.close()
+print "Successfully sent e-mail"
